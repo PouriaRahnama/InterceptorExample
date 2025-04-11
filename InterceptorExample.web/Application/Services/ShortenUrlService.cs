e8e9ea9ca155e2149db075657d56e8c0bdd4c5f4
Pouria <pouria.rahnama78@gmail.com>
2025-04-11 11:52:25 +0330

    Add project files.

---
+++ InterceptorExample.web/Application/Services/ShortenUrlService.cs
@@ -0,0 +1,43 @@
+﻿using InterceptorExample.web.Domain;
+using InterceptorExample.web.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace InterceptorExample.web.Application.Services
+{
+    public class ShortenUrlService
+    {
+        private readonly SqlServerApplicationDbContext _context;
+
+        public ShortenUrlService(SqlServerApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<string> CreateShortenLink(string destinationLink, CancellationToken cancellationToken)
+        {
+            string shortenCode = GenerateCode();
+            Link link = new()
+            {
+                shortenUrl = shortenCode,
+                destenationUrl = destinationLink
+            };
+            await _context.Links.AddAsync(link,cancellationToken);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return $"https://localhost:7084/{shortenCode}";
+        }
+
+        public string GenerateCode()
+        {
+            return Guid.NewGuid().ToString().Substring(0, 5);
+        }
+
+        public async Task<string> GetDestinationUrlAsync(string shortenCode, CancellationToken cancellationToken)
+        {
+            var destination = await _context.Links.FirstOrDefaultAsync(p => p.shortenUrl == shortenCode);
+
+            //if (destination is null) throw new ArgumentNullException(nameof(destination));
+            if (destination is null) return string.Empty;
+            return destination.destenationUrl;
+        }
+    }
+}
